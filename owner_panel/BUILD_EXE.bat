2021806bb7f89706777b3b60d6def0abe4ae4275
ryderwing <monkeydoodle600@gmail.com>
2026-08-31 18:42:00 -0400

    Delete owner_panel directory

--- owner_panel/BUILD_EXE.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-python -m pip install -r requirements.txt
-if errorlevel 1 pause & exit /b 1
-python -m PyInstaller --noconfirm --clean --windowed --onefile --name PentestHubOwner main.py
-if errorlevel 1 pause & exit /b 1
-echo Built: dist\PentestHubOwner.exe
-pause
